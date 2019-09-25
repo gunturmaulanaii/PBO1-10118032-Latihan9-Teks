16d0278884df540f56ea61b11438bca43a2cb297
Guntur Maulana Ibrahim <55784684+gunturmaulanaii@users.noreply.github.com>
2019-09-25 16:24:19 +0700

    Add files via upload

---
+++ PBO1-10118032-Latihan9-Teks/src/latihan/pkg9/teks/PBO110118032Latihan9Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg9.teks;
+
+/**
+ *
+ * @author
+ * NAMA  : Guntur Maulana Ibrahim
+ * Kelas : PBO1
+ * NIM   : 10118032
+ * Deskripsi Program : Program ini berisi untuk menampilkan teks dengan newline
+ */
+public class PBO110118032Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char : " + tab + "berisi " + c);
+    }
+    
+}
